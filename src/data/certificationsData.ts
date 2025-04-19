712725fbc0930c6a2cadbcb5361a488325d3437a
Jesus Rodolfo Gil farias <jrgil.20@est.ucab.edu.ve>
2025-04-19 18:41:44 +0000

    feat: Add new certification data for 'Github Basics'

--- src/data/certificationsData.ts
+++ src/data/certificationsData.ts
@@ -25,5 +25,15 @@ export const certificationsData: Certification[] = [
     skills: ['C (lenguaje de programación)', 'Desarrollo backend'],
     imageUrl: 'https://udemy-certificate.s3.amazonaws.com/image/UC-dbdd0133-33b9-47ce-b0d0-442f53dd7f03.jpg?v=1683449779000',
     certificateUrl: 'https://www.udemy.com/certificate/UC-dbdd0133-33b9-47ce-b0d0-442f53dd7f03/'
+  },
+  {
+    id: 'cert-2',
+    title: 'Github Basics',
+    provider: 'Udemy',
+    date: '23 de octubre de 2024',
+    description: 'Certificado de finalización de Udemy. Duración: 1 hora en total.',
+    skills: ['Control de versiones', 'Github'],
+    imageUrl: 'https://udemy-certificate.s3.amazonaws.com/image/UC-e05cdb24-ef21-46e7-b6ff-653e276ef402.jpg?v=1729722776000',
+    certificateUrl: 'https://www.udemy.com/certificate/UC-e05cdb24-ef21-46e7-b6ff-653e276ef402/'
   }
 ];
